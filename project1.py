df893b4866b487396a2f6f425ae4ec3962d82948
Justice <n-nyimock.bitanyanmi@stu.ucc.edu.gh>
2023-11-12 15:36:02 +0000

    my first code

--- project1.py
+++ project1.py
@@ -4,3 +4,11 @@ gn = "Good night"
 print(gm)
 print(ge)
 print(gn)
+
+# Path: project2.py
+def gm():
+    print("Good morning")
+def ge():
+    print("Good evening")
+def gn():
+    print("Good night")
